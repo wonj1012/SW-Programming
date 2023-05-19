f77b4636a3ce484f5bf3f240c21f201f352da0b6
wonj1012 <jjaa1012@gmail.com>
2023-05-19 14:23:35 +0900

    lab6 added

--- lab/lab_5.py
+++ lab/lab_5.py
@@ -2,73 +2,76 @@
 SW Programming Lab #5
 Name: 최원재
 ID: 2020147530
-Date: 2023-05-19
+Date: 2023-05-17
 """
 
-import numpy as np
-from typing import Tuple
-import matplotlib.pyplot as plt
-
-DIR = "data/"
+from functools import reduce
 
 
-def q1(file_path: str = f"{DIR}lab_5-1.txt") -> np.ndarray:
-    # generate random array and save to file
-    arr = np.random.randint(1, 46, size=(10, 10))
-    np.savetxt(file_path, arr, fmt="%d")
+def q1() -> None:
+    def sub(n1: int, n2: int) -> int:
+        return n1 - n2
 
-    # read file
-    with open(file_path, "r") as file:
-        lines = file.readlines()
-        # print file contents
-        for line in lines:
-            print(line.strip())
+    num1, num2 = map(int, input("두 수를 입력: ").split())
 
-    # convert to numpy array
-    arr = np.array([line.strip().split() for line in lines], dtype=int)
-    # print numpy array
-    print(arr)
-
-    return arr
+    print(f"{num1} - {num2} = {sub(num1, num2)}")
+    print(f"{num1} - {num2} = {(lambda x, y: x - y)(num1, num2)}")
 
 
-def q2(file_path: str = f"{DIR}lab_5-2.txt") -> Tuple[np.ndarray, np.ndarray]:
-    arr = q1(file_path)
-    row_sum = np.sum(arr, axis=1)
-    col_sum = np.sum(arr, axis=0)
-    len_arr = len(arr)
-    for i in range(len_arr):
-        for num in arr[i]:
-            print(f"{num:4d}", end=" ")
-        print(f"| {row_sum[i]:4d} | {row_sum[i] / len_arr:4.1f}")
-    print("-" * (len_arr * 5))
-    for i in range(len_arr):
-        print(f"{col_sum[i]:4d}", end=" ")
-    print()
-    print("-" * (len_arr * 5))
-    for i in range(len_arr):
-        print(f"{col_sum[i] / len_arr:4.1f}", end=" ")
-    print()
+def q2() -> None:
+    nums = list(range(1, 11))
 
-    return row_sum, col_sum
+    E1_list = []
+    for even_num in filter(lambda x: x % 2 == 0, nums):
+        E1_list.append(even_num)
+
+    E2_list = [x for x in filter(lambda x: x % 2 == 0, nums)]
+
+    print(f"E1_list = {E1_list}")
+    print(f"E2_list = {E2_list}")
 
 
-def q3(file_path: str = f"{DIR}lab_5-3.txt") -> None:
-    # generate graph of average of sum of rows and columns
-    row_sum, col_sum = q2(file_path)
-    row_avg = list(map(lambda x: x / len(row_sum), row_sum))
-    col_avg = list(map(lambda x: x / len(col_sum), col_sum))
+def q3() -> None:
+    def to_upper(s: str) -> str:
+        return s.upper()
 
-    X = range(1, len(row_avg)+1)
-    Y1 = row_avg
-    Y2 = col_avg
-    plt.plot(X, Y1, label="row average")
-    plt.plot(X, Y2, label="column average")
-    plt.xlabel("row/column number")
-    plt.ylabel("value")
-    plt.legend(loc="upper left")
-    plt.title("Average of sum of rows and columns")
-    plt.show()
+    a_list = input("소문자 입력: ").split()
+
+    A1_list = list(map(to_upper, a_list))
+    A2_list = list(map(lambda x: x.upper(), a_list))
+
+    print(f"a_list = {a_list}")
+    print(f"A1_list = {A1_list}")
+    print(f"A2_list = {A2_list}")
+
+
+def q4() -> None:
+    i_list = list(map(int, input("정수 입력: ").split()))
+
+    F1_list = list(map(float, i_list))
+    F2_list = list(map(lambda x: float(x), i_list))
+
+    print(f"F1_list = {F1_list}")
+    print(f"F2_list = {F2_list}")
+
+
+def q5() -> None:
+    a, b, n = map(int, input("a, b, n 입력: ").split())
+
+    ab_sum = reduce(lambda x, y: x + y, range(a, b + 1))
+    n_factorial = reduce(lambda x, y: x * y, range(1, n + 1))
+
+    print(f"{a} ~ {b}의 합 = {ab_sum}")
+    print(f"{n}! = {n_factorial}")
+
+
+def q6() -> None:
+    string = input("문자열 입력: ")
+    words = string.split()
+    first_letters = [word[0] for word in words]
+
+    print(f"입력된 문자열 = {words}")
+    print(f"첫번째 문자들 = {first_letters}")
 
 
 if __name__ == "__main__":
@@ -78,3 +81,9 @@ if __name__ == "__main__":
     q2()
     print("\n==================== Q3 ====================")
     q3()
+    print("\n==================== Q4 ====================")
+    q4()
+    print("\n==================== Q5 ====================")
+    q5()
+    print("\n==================== Q6 ====================")
+    q6()
