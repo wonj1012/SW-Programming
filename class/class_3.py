75bdfc791cbf9ed3fb7121979c8a1985bd9cc4d0
wonj1012 <jjaa1012@gmail.com>
2023-05-24 14:55:57 +0900

    add class_3

---
+++ class/class_3.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+import csv
+from typing import List
+
+
+def get_bs_obj(url: str) -> BeautifulSoup:
+    result = requests.get(url)
+    result.raise_for_status()
+    bs_obj = BeautifulSoup(result.content, "lxml")
+    return bs_obj
+
+
+def write_csv(data: List[List[str]], column_names: List[str], path: str) -> None:
+    with open(path, "w", encoding="utf-8") as f:
+        writer = csv.writer(f)
+        writer.writerow(column_names)
+        for row in data:
+            writer.writerow(row)
+
+
+if __name__ == "__main__":
+    soup_imdb = get_bs_obj("https://www.imdb.com/chart/top")
+
+    data = []
+    for tr in soup_imdb.find("tbody", {"class": "lister-list"}).find_all("tr"):
+        rank = tr.find("td", {"class": "titleColumn"}
+                       ).text.split()[0].strip(".")
+        title = tr.find("td", {"class": "titleColumn"}).find("a").get_text()
+        rating = tr.find("td", {"class": "ratingColumn imdbRating"}).find(
+            "strong").get_text()
+        year = tr.find("td", {"class": "titleColumn"}).find(
+            "span", {"class": "secondaryInfo"}).get_text().strip("()")
+        data.append([rank, title, rating, year])
+    write_csv(data=data, column_names=["rank", "title", "rating", "year"],
+              path="data/imdb_top250.csv")
