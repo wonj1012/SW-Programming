248ab9427f0fc386f254cbfa8a98f9ac241d8250
wonj1012 <jjaa1012@gmail.com>
2023-05-29 12:56:33 +0900

    docstring added to lab_7.py

--- lab/lab_7.py
+++ lab/lab_7.py
@@ -11,16 +11,30 @@ import csv
 
 
 def get_soup(url: str) -> BeautifulSoup:
+    """
+    Fetches the page content from the given URL and parses it using BeautifulSoup.
+
+    :param url: The URL of the webpage.
+    :return: A BeautifulSoup object representing the parsed webpage.
+    """
     response = requests.get(url)
     response.raise_for_status()
     return BeautifulSoup(response.text, 'lxml')
 
 
 class Q1:
+    """
+    A class to handle operations related to "https://movie.daum.net/ranking/reservation" webpage.
+    """
+
     def __init__(self):
         self.filepath = "data/lab_7/movies.csv"
 
     def solve(self) -> None:
+        """
+        Fetches movie data from the webpage, saves the data to a CSV file,
+        and prints the movie data.
+        """
         soup = get_soup("https://movie.daum.net/ranking/reservation")
 
         # Writing to the file
@@ -28,20 +42,22 @@ class Q1:
             writer = csv.writer(file)
             writer.writerow(["순위", "영화명",  "평점", "예매율"])
 
-            movies = soup.find_all("div", attrs={"class": "item_poster"})
-            for movie in movies:
-                rank = movie.find(
-                    "span", attrs={"class": "rank_num"}).get_text()
+            for movie in soup.find_all("div", class_="item_poster"):
+                rank = movie.find("span", class_="rank_num").get_text()
                 title = movie.find(
-                    "a", attrs={"data-tiara-layer": "moviename"}).get_text()
-                grade = movie.find(
-                    "span", attrs={"class": "txt_grade"}).get_text()
-                resrate = movie.find(
-                    "span", attrs={"class": "txt_num"}).get_text()[:-1]
+                    "a", {"data-tiara-layer": "moviename"}).get_text()
+                grade = movie.find("span", class_="txt_grade").get_text()
+                resrate = movie.find("span", class_="txt_num").get_text()[:-1]
 
                 writer.writerow([rank, title, grade, resrate])
 
         # Reading from the file
+        self.read_file()
+
+    def read_file(self) -> None:
+        """
+        Reads and prints the movie data from the CSV file.
+        """
         with open(self.filepath, "r", encoding="utf-8-sig") as file:
             reader = csv.reader(file)
             next(reader)  # Skip the header row
@@ -54,10 +70,18 @@ class Q1:
 
 
 class Q2:
+    """
+    A class to handle operations related to "https://www.imdb.com/title/tt0285331/episodes?season=" webpage.
+    """
+
     def __init__(self):
         self.filepath = "data/lab_7/imdb_episodes.csv"
 
     def solve(self) -> None:
+        """
+        Fetches episode data for each season from the webpage, saves the data to a CSV file,
+        and prints the episode data.
+        """
         # Open CSV file to write
         with open(self.filepath, 'w', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
@@ -68,22 +92,27 @@ class Q2:
                 soup = get_soup(url)
 
                 # For each episode
-                for episode in soup.find_all('div', {'class': 'list_item'}):
-                    season = episode.find(
-                        'div', {'class': 'image'}).find('div').text.strip().split(',')[0][1:]
-                    title = episode.find(
-                        'a', {'itemprop': 'name'}).text.strip()
+                for episode in soup.find_all('div', class_='list_item'):
+                    season = episode.find('div', class_='image').find(
+                        'div').text.strip().split(',')[0][1:]
+                    title = episode.find('a', itemprop='name').text.strip()
                     rating = episode.find(
-                        'span', {'class': 'ipl-rating-star__rating'}).text
+                        'span', class_='ipl-rating-star__rating').text
                     review_count = episode.find(
-                        'span', {'class': 'ipl-rating-star__total-votes'}).text.strip('()')
+                        'span', class_='ipl-rating-star__total-votes').text.strip('()')
                     synopsis = episode.find(
-                        'div', {'class': 'item_description'}).text.strip()
+                        'div', class_='item_description').text.strip()
 
                     writer.writerow(
                         [season, title, review_count, rating, synopsis])
 
         # Reading from the file
+        self.read_file()
+
+    def read_file(self) -> None:
+        """
+        Reads and prints the episode data from the CSV file.
+        """
         with open(self.filepath, "r", encoding="utf-8-sig") as file:
             reader = csv.reader(file)
             next(reader)  # Skip the header row
